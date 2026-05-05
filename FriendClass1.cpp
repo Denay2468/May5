42812316c5c88a9d78cd1db92adeca051e144caa
Denay2468 <denayaung13@gmail.com>
2026-05-05 09:55:56 +0630

    create class student and public and private variable inside

--- FriendClass1.cpp
+++ FriendClass1.cpp
@@ -12,3 +12,13 @@ public:
     void setName(string pName);
     friend class student;
 };
+
+class student
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(person &a);
+};
