7d0f89788a8bb03fe3d68a36b6711b38b33c674f
Denay2468 <denayaung13@gmail.com>
2026-05-05 10:07:46 +0630

    create main function and fucntion call inside

--- Pointer1.cpp
+++ Pointer1.cpp
@@ -7,3 +7,17 @@ class collegeStudent{
         cout<<"Student ID = "<<studentId<<endl;
     }
 };
+
+int main (){
+    collegeStudent mhs{1}; // Object mhs
+    mhs.showStudentId(); //Member Access Operator
+
+    collegeStudent &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.studentId = 2; //Member Access Operator
+    mhs.showStudentId();
+
+    collegeStudent *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->studentId = 3; //Arrow Operator
+    pMhs->showStudentId();
+    return 0;
+}
